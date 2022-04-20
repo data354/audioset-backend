f1510e782f469bb50c156f7962f7cea15e134ac7
Dy Koffi <dykoffi2000@gmail.com>
2022-04-20 18:48:39 +0000

    test(controler): Add test agent to Home controller

--- src/modules/home/home.test.ts
+++ src/modules/home/home.test.ts
@@ -1,14 +1,15 @@
-import { API_URL } from "../../configs/constants";
+import app from "../../server";
 
 describe("Home routes tester", () => {
 
-    const supertest = require("supertest")(API_URL);
+    const supertest = require("supertest")(app);
 
     it("/GET get test", async () => {
 
         let res = await supertest
             .get("/")
             .expect("Content-Type", /json/)
+            .expect(200)
 
         expect(res.status).not.toBe(500);
 
