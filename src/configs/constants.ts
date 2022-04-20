99696f8b804f2237a8ec0503407953e5cdca9aa0
Dy Koffi <dykoffi2000@gmail.com>
2022-04-20 18:59:52 +0000

    refactor(test): use globalroutes file instead index file

--- src/configs/constants.ts
+++
@@ -1,3 +0,0 @@
-require("dotenv").config({ path: require("path").resolve(process.cwd(), ".info.env") });
-
-export const API_URL = `${process.env.PROTOCOL}://${process.env.HOST}:${process.env.PORT}`
