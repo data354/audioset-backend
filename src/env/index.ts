b7a42b095fe9dd8bc9b77fb03058a028d01a0d9b
Dy Koffi <dykoffi2000@gmail.com>
2022-04-22 15:47:17 +0000

    fix: Bug secret json key

--- src/env/index.ts
+++ src/env/index.ts
@@ -1,4 +1,15 @@
 import secret from "../../secrets.json"
-let secret_env = secret[process.env.NODE_ENV || "local"]
+
+let env: string
+let node_env = process.env.NODE_ENV
+
+if (node_env && Object.keys(secret).includes(node_env)) {
+    env = node_env
+} else {
+    env = "local"
+}
+
+
+let secret_env = secret[env]
 
 export default secret_env
